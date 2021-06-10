98311803a63459206ef184d674284455889d05df
ACO <ocampoalejandro.oc@gmaiul.com>
2021-06-10 14:50:39 -0500

    Comentarios agregados al código

--- criptonita.sh
+++ criptonita.sh
@@ -85,9 +85,9 @@ validarRuta()
 				clear
 			;;
 			"exit")  #Salimos del prompt de validación de ruta
-				break
+				break #Salimos de prompt por indicaciones de usuario
 			;;			
-			*)
+			*) #En el caso en el que no se haya ingreado un comando o en caso de errores
 				#De no estar en el listado de comandos pasa a ruta para su próxima verificación 
 				RUTA="$COM_RUT"
 
@@ -97,13 +97,13 @@ validarRuta()
 				if [ -f "$RUTA" ] #En el caso de que exista la ruta y se un fichero
 				then
 					#Existe únicamente el fichero										
-					FILE="TRUE"
-					ARCHIVO="TRUE"															
+					FILE="TRUE"  #Se indica que existe el fichero
+					ARCHIVO="TRUE" #Se incdica que el se tiene un archivo para trabajar							
 				elif [ -d "$RUTA" ] #En el caso de que exita la ruta y el directorio
 				then
 					#Existe únicamente el direct
-					DIR="TRUE"
-					ARCHIVO="TRUE"
+					DIR="TRUE"  #Se indica que existe el directorio
+					ARCHIVO="TRUE" #Se indica que se tiene un archivo de trabajo
 				else
 					#La ruta que ingresó no es una ruta válida
 					echo "La ruta es incorrecta"
@@ -154,16 +154,19 @@ cifrado()
 				mv $RUTA.gpg $RUTA #cambia el nombre con extensión original
 			;;
 			"6") #Para cifrar a CAMELIA128
-				gpg --no-symkey-cache --symmetric --cipher-algo CAMELIA128 $RUTA && rm $RUTA #Cifra genera un gpg y elimina original
-				mv $RUTA.gpg $RUTA #cambia el nombre con extensión original
+				echo "Algoritmo no disponible aún"
+				#gpg --no-symkey-cache --symmetric --cipher-algo CAMELIA128 $RUTA && rm $RUTA #Cifra genera un gpg y elimina original
+				#mv $RUTA.gpg $RUTA #cambia el nombre con extensión original
 			;;
 			"7") #Para cifrar a CAMELIA192
-				gpg --no-symkey-cache --symmetric --cipher-algo CAMELIA192 $RUTA && rm $RUTA #Cifra genera un gpg y elimina original
-				mv $RUTA.gpg $RUTA #cambia el nombre con extensión original
+				echo "Algoritmo no disponible aún"
+				#gpg --no-symkey-cache --symmetric --cipher-algo CAMELIA192 $RUTA && rm $RUTA #Cifra genera un gpg y elimina original
+				#mv $RUTA.gpg $RUTA #cambia el nombre con extensión original
 			;;
 			"8") #Para cifrar a CAMELIA256
-				gpg --no-symkey-cache --symmetric --cipher-algo CAMELIA256 $RUTA && rm $RUTA #Cifra genera un gpg y elimina original
-				mv $RUTA.gpg $RUTA #cambia el nombre con extensión original
+				echo "Algoritmo no diponible aún"
+				#gpg --no-symkey-cache --symmetric --cipher-algo CAMELIA256 $RUTA && rm $RUTA #Cifra genera un gpg y elimina original
+				#mv $RUTA.gpg $RUTA #cambia el nombre con extensión original
 			;;
 			"9") #Para cifrar a CAST5
 				gpg --no-symkey-cache --symmetric --cipher-algo CAST5 $RUTA && rm $RUTA #Cifra genera un gpg y elimina original
@@ -195,31 +198,38 @@ cifrado()
 
 descifrar()
 {
-
-	validarRuta
-	RESPUESTA=""
-	if [ $FILE = "TRUE" ] && [ $ARCHIVO = "TRUE" ]
+	#Se manda a llamar cuando en el menú principal  es seleccionada la opción 2) descifrar
+	validarRuta # se pide una ruta para encontrar el archivo a descifrar y se valida la ruta
+	RESPUESTA="" #Variable que va a almacenar la respuesta a la pregunta de si se trata de un directorio 
+	if [ $FILE = "TRUE" ] && [ $ARCHIVO = "TRUE" ] #Si el archivo existe y es un fichero entonces
 	then
+		#Entramos a una pregunta que solo deja de hacerse cuando es respondida con si o no para saber que hacer con el archivo
 		until [ "$RESPUESTA" == "s" ] || [ "$RESPUESTA" == "S" ] || [ "$RESPUESTA" == "N" ] || [ "$RESPUESTA" == "n" ]
 		do
-			echo "¿El archivo a descifrar era un directorio S/N?"
-			read RESPUESTA			
+			echo "¿El archivo a descifrar era un directorio S/N?" #Se pregunta al usuario si era fichero o directorio esto 
+			#se debe a que el archivo al estar cifrado no sabe si se trataba de un fichero o directorio
+			read RESPUESTA	# Se almacena la respuesta del usuario
 		done
-		if [ $RESPUESTA = "S" ] || [ $RESPUESTA = "s" ]
-		then
+		if [ $RESPUESTA = "S" ] || [ $RESPUESTA = "s" ] #Es el caso de que el usuario ponga "S o s " procedemos 
+		then # a descifrar el archivo como una carpeta haciendo el paso de descompresión del directrio
+			#primero cambiamos el nombre a gpg para descomprimir, en caso de fallar se regresa a extensión normal
 			mv $RUTA $RUTA.gpg && gpg -d -o $RUTA $RUTA.gpg || mv $RUTA.gpg $RUTA 
-			if [ -f "$RUTA" ]
+			if [ -f "$RUTA" ] # en el caso de que el archivo se hay descifrado y exista 
 			then
-				RUTAACTUAL="$(pwd)"
-				MADREDIR="$(cd $RUTA && cd .. && pwd)"
-				rm $RUTA.gpg && mv $RUTA $RUTA.tar && cd $MADREDIR && tar -xvf $RUTA.tar && rm $RUTA.tar 
-				cd $RUTAACTUAL
+				RUTAACTUAL="$(pwd)" #obtenemos la ruta actual para poder regresar cuando nos movamos a la carpeta contenedora
+				MADREDIR="$(cd $RUTA && cd .. && pwd)" #Identificamos la carpeta contenedora del archivo
+				rm $RUTA.gpg && mv $RUTA $RUTA.tar && cd $MADREDIR && tar -xvf $RUTA.tar && rm $RUTA.tar  
+				#Con la línea anterior desempaquetamos los en la ruta contenedora del directorio a descifrar 
+				cd $RUTAACTUAL #regresamos a la posición original
 			fi
-		elif [ $RESPUESTA = "N" ] || [ $RESPUESTA = "n" ]
-		then	 
-			mv $RUTA $RUTA.gpg && gpg -d -o $RUTA $RUTA.gpg || mv $RUTA.gpg $RUTA
+		elif [ $RESPUESTA = "N" ] || [ $RESPUESTA = "n" ]  #Es el caso en el que tenemos un fichero
+		then	  
+			#Se cambia el nombre a gpg se descifra de no ser posible se regres a la extensión anterior
+			mv $RUTA $RUTA.gpg && gpg -d -o $RUTA $RUTA.gpg || mv $RUTA.gpg $RUTA 
+			#La siguiente es una condición que permite verificar si el arcivo gpg y el descifrado se generaron
 			if [ -f "$RUTA.gpg" ] && [ -f "$RUTA" ]
 			then
+				#eliminamos el gpg pues ya tenemos el descifrado
 				rm $RUTA.gpg 
 			fi
 		fi	
@@ -230,21 +240,21 @@ descifrar()
 
 
 #Verificamos que exista steghide
-if [ -f "/usr/bin/steghide" ]
+if [ -f "/usr/bin/steghide" ] # consultamos si está instalado steghide en su sistema
 then
-	echo "Steghide OK"
+	echo "Steghide OK" #Indica que si se encuentra
 else
-	echo "Requiere de instalación de steghide"
-	sudo apt install steghide
+	echo "Requiere de instalación de steghide" 
+	sudo apt install steghide #Instalación de steghide para apt
 fi
 
 #Verificamos que exista gpg 
 if [ -f "/usr/bin/gpg" ]
 then
-	echo "gpg OK"
+	echo "gpg OK"  #Indica que el quipo cuenta con gpg
 else
 	echo "Requiere instalación de gpg"
-	sudo apt install gpg
+	sudo apt install gpg #En caso de no tener gpg el equipo se instala
 fi
 
 #Limpiamos pantalla para comenzar con ejecución de script
@@ -256,13 +266,17 @@ OPCION="0";
 #Bucle de menú principal, se rompe al usar 4, exit, salir o 4)
 until [ "$OPCION" == "4" ] || [ "$OPCION" == "exit" ] || [ "$OPCION" == "salir" ] || [ "$OPCION" == "4)" ]
 do
-	echo "criptonita 0.0.1 GPL v3"
+	#Este es el menú principal de opciones
+	echo -e "${C}criptonita 0.0.1 GPL v3 ${NC}"
 	echo -e "Seleccione la opción deseada:"
-	echo "1) Cifrar"
-	echo "2) Descifrar"
-	echo "3) Esteganografía"
+	echo "1) Cifrado simétrico"
+	echo "2) Descifrado simétrico"
+	echo "3) Cifrado ásimétrico"
 	echo "4) Salir"
+
+	#Se lee la opción indicada por el usuario
 	read OPCION;
+
 	#switch case que nos perminte actuar de acuerdo a la respuesta de menú principal
 	case $OPCION in
 		"1")						
@@ -270,8 +284,9 @@ do
 			#METODO es la variable que almacena la respuesta del usuario en el menú de métodos de cifrado
 			METODO="0";
 			#Bucle del menú de métodos, se rompe con selección de respuesta 8
-			until [ "$METODO" == "12" ]
+			until [ "$METODO" == "12" ] #Nos mantenemos en el menú a menos que el usuario indique la opción "regresar"
 			do
+				#Estos son los métodos de cifrado simétricos con los que puede trabajar criptonita
 				clear								
 				echo -e "Selecciona el método de cifrado:"
 				echo "1) 3DES"
@@ -279,31 +294,43 @@ do
 				echo "3) AES 192"
 				echo "4) AES 256"
 				echo "5) BLOWFISH"
-				echo "6) CAMELIA 128"
-				echo "7) CAMELIA 192"
-				echo "8) CAMELIA 256"
+				echo "6) CAMELIA 128 (En desarrollo)" #Aún no terminadas
+				echo "7) CAMELIA 192 (En desarrollo)" #Aún no terminadas
+				echo "8) CAMELIA 256 (En desarrollo)" #Aún no terminadas
 				echo "9) CAST5"
 				echo "10) IDEA"
 				echo "11) TWOFISH" 
 				echo "12) regresar"
+				#Leemos el método indicado por el usuario
 				read METODO
 				
+				#Aquí se verifica que el método que selecciona el usuario se encuentra en la lista
 				if [ "$METODO" -ge 1 ] && [ "$METODO" -le 11 ]
 				then
-					cifrado
+					#Se aplica la función cifrado
+					cifrado 
+					#una vez salido de la función cifrado no puede haber recursividad a menos que se vuelva a entrar
+					#Esta asignación aquí asegura que no haya una recursividad no desada en caso de volver a cifrar
 					RECURSIVO="FALSE"
-					clear
-				elif [ "$METODO" = "12" ]
+					clear	
+				elif [ "$METODO" = "12" ] # en el caso de que el usario quiera regresar al menú principal
 				then
-					clear
+					clear 
 				fi
 			done
 			;;
-		"2")
-			descifrar
+		"2") #Este es el caso en el que el usuario quiere descifrar simétricamente
+			descifrar && clear && echo "Descifrado completado" #Se llama a la función descifrar 
 			;;
-		"3")
-			echo "Seleccionaste Esteganografía"
+		"3") #Esta es la opción para algoritmos de cifrado asimétrico
+			echo -e "Generación de claves"
+			echo "Para cifrar asimétricamente, primero tenemos que crar tu par de claves: "
+			echo "ENTER/aceptar"
+			#Leemos un enter con:
+			read -s -n 1 key #No se permite la escritira, solo se lee un caracter separado con un espacio
+
+
+
 			;;
 		"4")
 			clear
