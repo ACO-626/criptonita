584f1f6aa1c5d13a7ae9ec54e4c9fd7392a9b22d
ACO <ocampoalejandro.oc@gmaiul.com>
2021-06-09 20:17:24 -0500

    Se agregó cifrado de directorio y color al prompt

--- criptonita.sh
+++ criptonita.sh
@@ -1,164 +1,193 @@
 #!/bin/bash
 
+#criptonita 0.0.1 GPLv3
+
+#This Shell Script uses bash as command interpreter / Este script utiliza bash como interprete de comandos
 #Author: ACO
 #Date: 2021
-#This Shell Script uses bash as command interpreter
 #Commit language: Spanish
+#contact:acoproteco@gmail.com
 
-#Funcion de validado de ruta
+
+
+#Variables para imprimir texto en color se usa en algunos textos junto con echo 
+C='\033[0;31m'  #Variable para colorear texto
+NC='\033[0m'    #Variable para quitar color a texto
+
+
+
+
+#Funcion que verifica si la ruta de archivo es válida
 validarRuta()
 {
-	#Variable que indica si ya se tiene un objeto a cifrar
+	#Variable que almacena si ya se encontró el archivo a cifrar o descifrar
 	ARCHIVO="FALSE"
-	#Se imprimen las instrucciones para selección de ruta de archivo a cifrar
-	clear;
-	echo "INGRESE LA RUTA, PUEDE USAR COMANDOS BÁSICOS DE NAVEGACIÓN"
+	FILE="FALSE" # FILE es True cuando sabemos que se trata de un fichero
+	DIR="FALSE"  #DIR  es True cuando sabemos que se trata de una carpeta
+
+	#Se imprimen las instrucciones para selección de ruta de archivo se puede navegar por los directorios
+	clear; #Limpiamos emulador de consola
+	echo "INGRESE LA RUTA, PUEDE APOYARSE EN LOS COMANDOS"
 	echo "Listar: list"
 	echo "Regresar: back"
-	echo -e "Dirigirse a (poner go ENTER y luego la ruta): go"
-	echo  "Para ingresar la ruta del archivo solo escribala"
-	#Bucle que se rompe cuando se indica una ruta aceptada
+	echo -e "Dirigirse a (poner go ENTER y luego la ruta): go" #Texto con salto de línea 
+	echo  "Para ingresar la ruta del archivo solo escribala sin ningún comando previo" 
+	
+	#Bucle que mantiene en un prompt del script se rompe cuando se valida la ruta
 	until [ "$ARCHIVO" == "TRUE" ]
 	do							
-		echo -e "ESTAS EN: $PWD"
-		echo -n '>> '
-		#Variable que almacena la ruta o el comando para el archivo a cifrar
-		read COM_RUT;
+
+		#Imprimimos la ruta en la que se encuentra el usuario
+		echo -e "${C}RUTA ACTUAL: $PWD ${NC}"
+
+		#Imprimimos un promt para indicar la entrada de comandos
+		echo -n 'criptonita@>> '
+		
+		#Variable que almacena la ruta o el comando de navegación
+		read COM_RUT; #Leemos el comando o ruta ingresada por el usuario
+
 		#Switch case para comandos de navegación o ingreso de ruta a archivo a cifrar
 		case $COM_RUT in
-			"list")
+			"list") #Listamos lo que hay en la ruta actual
 				#Listamos
 				ls;
 			;;
-			"back")
+			"back") #Es para poder regresar a la carpeta madre
 				#Tal cual es un cd ..
 				cd ..
 			;;	
-			"go")
+			"go") #Es como el comando cd pero a dos pasos
 				#Se almacenará la ruta para después dirigirse e ella
 				RUTA=""
 				echo -n "Ir a: "
+				
+				#Variable que almacena la ruta lee la entrada del usuario
 				read RUTA
+
+				#Verificamos que se trate de un directorio
 				if [ -d "$RUTA" ]
 				then
+					#Nos movemos al directorio en caso de existir
 					cd "$RUTA"
 				else
+					#Se indica que no es un directorio lo que indica que no podemos movernos ahí
 					echo "$RUTA no es un directorio"
 				fi
 			;;
-			"help")
+			"help") #Imprime de nuevo las instrucciones de uso
 				echo "INGRESE LA RUTA, PUEDE USAR COMANDOS BÁSICOS DE NAVEGACIÓN"
 				echo "Listar: list"
 				echo "Regresar: back"
 				echo -e "Dirigirse a (poner go ENTER y luego la ruta): go"
 				echo  "Para ingresar la ruta del archivo a cifrar solo escribala"
 			;;
-			"clear")
+			"clear") #Funciona como un clear en consola
 				clear
 			;;
-			"exit")		
+			"exit")  #Salimos del prompt de validación de ruta
 				break
-			;;
+			;;			
 			*)
-				#Se parte de la consideración que el usuario puso una ruta
+				#De no estar en el listado de comandos pasa a ruta para su próxima verificación 
 				RUTA="$COM_RUT"
-				#Variables a usar como booleanos para verificar que tipo de ruta ingresaron
-				FILE="FALSE"
-				DIR="FALSE"									
-				if [ -d "$RUTA" ] && [ -f "$FILE" ]
-				then
-					#En el caso que exista el fichero y el directorio
-					echo "La ruta es ambigua, usted se refiere a:"
-					echo "1) Directorio"
-					echo "2) Documento"
-					#Variable para ambiguedad
-					DESAMB=""
-					read DESAMB
-					if [ DESAMB = "1" ]
-					then
-						DIR="TRUE"
-						ARCHIVO="TRUE"
-					elif [ DESAMB = "2" ]
-					then
-						FILE="TRUE"
-						ARCHIVO="TRU"
-					else
-						echo "Respuesta no válida"
-					fi
-				elif [ -f "$RUTA" ]
+
+				#Variables a usar como booleanos para verificar que tipo de ruta ingresaron				
+
+				#Estructura de control de validación de ruta
+				if [ -f "$RUTA" ] #En el caso de que exista la ruta y se un fichero
 				then
 					#Existe únicamente el fichero										
 					FILE="TRUE"
 					ARCHIVO="TRUE"															
-				elif [ -d "$RUTA" ]
+				elif [ -d "$RUTA" ] #En el caso de que exita la ruta y el directorio
 				then
 					#Existe únicamente el direct
 					DIR="TRUE"
+					ARCHIVO="TRUE"
 				else
+					#La ruta que ingresó no es una ruta válida
 					echo "La ruta es incorrecta"
 				fi
 			;;
-		esac
-	done
+		esac #Final de switch para comandos de validación de ruta
+	done #Final de ciclo de busqueda de archivo a trabajar 
 }
 
 #FUNCIÓN DE CIFRADO
 cifrado()
 {	
 	#Se investiga la ruta del archivo
-	validarRuta
-	
-	
+	if [ $RECURSIVO = "TRUE" ] #El caso recursivo es exclusivo para el segundo tratamiento de directorio
+	then
+		RECURSIVO = "FALSE"  #El recursivo solo debe aplicar una vez por cada directorio
+		DIR = "FALSE"        #Se indica que ya no hay un directorio al cual cifrar 
+	else
+		#Se pide una ruta para indicar el archivo y se valida 
+		#(en el caso de recursivo la ruta ya se tiene y no requiere validación extra) 
+		validarRuta          
+	fi	
+
 	#Verificamos que tipo de ruta dio el usuario
-	if [ $FILE = "TRUE" ]
+	if [ $FILE = "TRUE" ] && [ $ARCHIVO = "TRUE" ]
 	then	
-	case $METODO in
-		"1")
-			gpg --symmetric --cipher-algo 3DES $RUTA && rm $RUTA
-			mv $RUTA.gpg $RUTA
-		;;
-		"2")
-			gpg --symmetric --cipher-algo AES $RUTA && rm $RUTA
-			mv $RUTA.gpg $RUTA
-		;;
-		"3")
-			gpg --symmetric --cipher-algo AES192 $RUTA && rm $RUTA
-			mv $RUTA.gpg $RUTA
-		;;
-		"4")
-			gpg --symmetric --cipher-algo AES256 $RUTA && rm $RUTA
-			mv $RUTA.gpg $RUTA
-		;;
-		"5")
-			gpg --symmetric --cipher-algo BLOWFISH $RUTA && rm $RUTA
-			mv $RUTA.gpg $RUTA
-		;;
-		"6")
+	
+	#De acuerdo al método seleccionamos los comandos acordes
+		case $METODO in
+			"1") #Para cifrar a 3DES
+				gpg --symmetric --cipher-algo 3DES $RUTA && rm $RUTA
+				mv $RUTA.gpg $RUTA
+			;;
+			"2") #Para cifrar a AES 128
+				gpg --symmetric --cipher-algo AES $RUTA && rm $RUTA
+				mv $RUTA.gpg $RUTA
+			;;
+			"3") #Para cifrar a AES 192
+				gpg --symmetric --cipher-algo AES192 $RUTA && rm $RUTA
+				mv $RUTA.gpg $RUTA
+			;;
+			"4") #Para cifrar a AES 256
+				gpg --symmetric --cipher-algo AES256 $RUTA && rm $RUTA
+				mv $RUTA.gpg $RUTA
+			;;
+			"5") #Para cifrar a BLOWFISH
+				gpg --symmetric --cipher-algo BLOWFISH $RUTA && rm $RUTA
+				mv $RUTA.gpg $RUTA
+			;;
+			"6") #Para cifrar a CAMELIA128
 				gpg --symmetric --cipher-algo CAMELIA128 $RUTA && rm $RUTA
 				mv $RUTA.gpg $RUTA
-		;;
-		"7")
-			gpg --symmetric --cipher-algo CAMELIA192 $RUTA && rm $RUTA
-			mv $RUTA.gpg $RUTA
-		;;
-		"8")
-			gpg --symmetric --cipher-algo CAMELIA256 $RUTA && rm $RUTA
-			mv $RUTA.gpg $RUTA
-		;;
-		"9")
-			gpg --symmetric --cipher-algo CAST5 $RUTA && rm $RUTA
-			mv $RUTA.gpg $RUTA
-		;;
-		"10")
-			gpg --symmetric --cipher-algo IDEA $RUTA && rm $RUTA
-			mv $RUTA.gpg $RUTA
-		;;
-		"11")
-			gpg --symmetric --cipher-algo TWOFISH $RUTA && rm $RUTA
-			mv $RUTA.gpg $RUTA
-		;;
-		esac
-	fi		
+			;;
+			"7") #Para cifrar a CAMELIA192
+				gpg --symmetric --cipher-algo CAMELIA192 $RUTA && rm $RUTA
+				mv $RUTA.gpg $RUTA
+			;;
+			"8") #Para cifrar a CAMELIA256
+				gpg --symmetric --cipher-algo CAMELIA256 $RUTA && rm $RUTA
+				mv $RUTA.gpg $RUTA
+			;;
+			"9") #Para cifrar a CAST5
+				gpg --symmetric --cipher-algo CAST5 $RUTA && rm $RUTA
+				mv $RUTA.gpg $RUTA
+			;;
+			"10") #Para cifrar a IDEA
+				gpg --symmetric --cipher-algo IDEA $RUTA && rm $RUTA
+				mv $RUTA.gpg $RUTA
+			;;
+			"11") #Para cifrar a TWOFISH
+				gpg --symmetric --cipher-algo TWOFISH $RUTA && rm $RUTA
+				mv $RUTA.gpg $RUTA
+			;;
+		esac #Fin de switch de metodo de cifrado
+	elif [ $DIR = "TRUE" ] && [ $ARCHIVO = "TRUE" ] #Es el caso en el que el archivo es un directorio
+	then
+		#Para trabajarlo como archivo convertimos el directorio a un archivo.tar
+		#Cambianos la extensión .tar 
+		#Se llama de nuevo a la función cifrado para cifrar al Directorio que ahora es un fichero
+		#Se usa en el if para ver si se hace validación de ruta		 
+		#Se aplica recursividad
+		tar -cvf $RUTA.tar /$RUTA && rm -r $RUTA && mv $RUTA.tar $RUTA  && FILE="TRUE" && RECURSIVO="TRUE" && cifrado
+	fi
+
 }
 
 descencriptar()
@@ -235,6 +264,7 @@ do
 				if [ "$METODO" -ge 1 ] && [ "$METODO" -le 11 ]
 				then
 					cifrado
+					RECURSIVO="FALSE"
 					clear
 				elif [ "$METODO" = "12" ]
 				then
